6f531a52b564bca51c780453fbde563334193d98
shachard46 <shachadavid2013@gmail.com>
2026-05-31 23:32:42 +0300

    increaced timeout for aiq

--- config/trading_constants.py
+++ config/trading_constants.py
@@ -17,7 +17,7 @@ BANKROLL_USD = 1000.0  # allocation_usd = f * BANKROLL_USD
 # A-IQ Service (overridable via env vars AIQ_BASE_URL, AIQ_POLL_INTERVAL_SEC, AIQ_TIMEOUT_SEC)
 AIQ_BASE_URL = "http://localhost:8000"
 AIQ_POLL_INTERVAL_SEC = 2.0
-AIQ_TIMEOUT_SEC = 120.0
+AIQ_TIMEOUT_SEC = 600.0
 
 # Filter Thresholds
 FILTERS = {
